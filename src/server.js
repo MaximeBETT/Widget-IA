eea0eca944bd7cda0cc16f5b8ce4df43b554c20b
Maxime BETTON <156648557+MaximeBETT@users.noreply.github.com>
2026-02-22 18:13:40 +0100

    ajout de gh page en allow

--- src/server.js
+++ src/server.js
@@ -18,7 +18,8 @@ const allowedOrigins = [
   'http://www.webetton.fr',
   'https://widget-ia-8d1d.onrender.com',
   'http://localhost:3000',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
+  'https://maximebett.github.io'
 ];
 
 // Middleware
